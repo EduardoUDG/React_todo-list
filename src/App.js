eafe01c09fdac4d56ebc0da5423bbcb27cd65676
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2022-01-04 13:24:03 -0600

    create - Create form for add todo to tasks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
+import TaskForm from './components/TaskForm';
 
 
 const App = () => {
+  const [tasks, updateTasks] = useState([
+    {
+        id: new Date().getTime(),
+        text: 'Wash the clothes',
+        completed: false
+    },
+    {
+        id: new Date().getTime(),
+        text: 'record the tutorial',
+        completed: false
+    }
+  ]);
+
   return (
     <div className='contenedor'>
       <Header/>
+      <TaskForm tasks={tasks} updateTasks={updateTasks} />
     </div>
   );
 }
---
+++ src/components/TaskForm.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { v4 as uuidv4 }    from 'uuid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlusSquare }    from '@fortawesome/free-solid-svg-icons'
+
+const TaskForm = ({ tasks, updateTasks}) => {
+    const [ taskInput, updateTaskInput] = useState('');
+
+    const handleInput = e => {
+        updateTaskInput( e.target.value );
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        
+        updateTasks([ 
+            ...tasks,
+            {
+            id: uuidv4(),
+            text: taskInput,
+            completed: false
+            }
+        ]);
+    }
+
+    return (
+        <form action='' className='formulario-tareas' onSubmit={handleSubmit}>
+            <input 
+                type='text'
+                className='formulario-tareas__input'
+                placeholder='Write one task'
+                value={taskInput}
+                onChange={handleInput}
+            />
+            <button type='submit' className='formulario-tareas__btn'>
+                <FontAwesomeIcon icon={ faPlusSquare } className='formulario-tareas__icono-btn'/>
+            </button>
+        </form>
+    );
+}
+ 
+export default TaskForm;
