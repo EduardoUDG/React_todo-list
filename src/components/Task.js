a9c2ecc4a9eebc6b782c3ee14ec59b026c6a12ca
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2022-01-06 10:52:40 -0600

    update - function for the edited task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheckSquare, faEdit, faTimes  }    from '@fortawesome/free-solid-svg-icons'
 
 const Task = ({ task={} }) => {
+    const [ editingTask, changeEditingTask ] = useState(false);
+    const [ newTask, changingTask ] = useState( task.text);
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        changeEditingTask(false);
+    };
+
     return (
         <li className='lista-tareas__tarea'>
             <FontAwesomeIcon 
                 icon={faCheckSquare} 
                 className='lista-tareas__icono lista-tareas__icono-check'
             />
-
-            <p className='lista-tareas__texto'>
-                { task.text }
-            </p>
+            <div className='lista-tareas__texto'>
+                {   editingTask ?
+                    <form className='formulario-editar-tarea' onSubmit={ handleSubmit }>
+                        <input 
+                            type='text' 
+                            className='formulario-editar-tarea__input' 
+                            value={ newTask }
+                            onChange={ (e) => changingTask( e.target.value ) }
+                        />
+                        <button 
+                            type='submit'
+                            className='formulario-editar-tarea__btn'
+                        >
+                            Actualizar    
+                        </button>                        
+                    </form>
+                    : task.text 
+                }
+            </div>
             <div className='lista-tareas__contenedor-botones'>
                 <FontAwesomeIcon 
                     icon={faEdit} 
                     className='lista-tareas__icono lista-tareas__icono-accion'
+                    onClick={ () => changeEditingTask(!editingTask) }
                 />
                 <FontAwesomeIcon 
                     icon={faTimes} 
