e1885b9f2f48f77cb0d748704309338149fc276e
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2022-01-13 12:29:52 -0600

    update - add localStorage for tasks and show taks completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import TaskForm from './components/TaskForm';
@@ -6,20 +6,25 @@ import TaskList from './components/TaskList';
 
 
 const App = () => {
-  const [tasks, updateTasks] = useState([
-    {
-      id: 1,
-      text: 'A sinmple task',
-      completed: false
-    },
-    {
-      id: 2,
-      text: 'A sinmple task 2',
-      completed: true
-    }
-  ]);
 
-  const [showTaskCompleted, updateShowTaskCompleted] = useState(false);
+  // Save task in localStorage
+  const tasksSaved = localStorage.getItem('tasks') ?
+                      JSON.parse(localStorage.getItem('tasks')) 
+                      : [] ;
+  const [tasks, updateTasks] = useState( tasksSaved );
+  useEffect(()=>{
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  }, [tasks]);
+
+
+  // Save state of tasks completed
+  const saveTasksCompleted = localStorage.getItem('showTasksCompleted') ?
+                             JSON.parse(localStorage.getItem('showTasksCompleted'))
+                             : true;
+  const [showTaskCompleted, updateShowTaskCompleted] = useState(saveTasksCompleted);
+  useEffect(()=>{
+    localStorage.setItem('showTasksCompleted', showTaskCompleted.toString());
+  }, [showTaskCompleted]);
 
   return (
     <div className='contenedor'>
